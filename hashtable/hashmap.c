e43b4f354efe63e6029ea301a0fb1d73b5d08166
brainuser5705 <codeuser5705@outlook.com>
2024-03-11 12:52:16 -0400

    hashmap fix

--- hashtable/hashmap.c
+++ hashtable/hashmap.c
@@ -21,7 +21,7 @@ int hash_value(void *key);
 
 hashmap hashmap_create(){
     hashmap map = (hashmap)malloc(sizeof(struct hashmap_struct));
-    map->arr = (node *)malloc(sizeof(node) * HASHMAP_CAPACITY);
+    map->arr = (node *)calloc(HASHMAP_CAPACITY, sizeof(node));
     map->size = 0;
     return map;
 }
@@ -44,15 +44,13 @@ void *hashmap_get(hashmap map, void *key);
 void hashmap_remove(hashmap map, void *key);
 
 void hashmap_destroy(hashmap map){
-    node *prev;
-    node *n = map->arr;
-    while (n) {
-        prev = n;
-        destroy_node(prev);
-        free(prev);
-        n++;
-    }
-    free(map);
+  node *curr = map->arr;
+  while (curr) {
+    destroy_node(*curr);
+    curr++;
+  }
+  free(map->arr);
+  free(map);
 }
 
 void hashmap_print(hashmap map){
